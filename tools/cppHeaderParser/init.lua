3d8e16a23062b0426cb5bf9696aea2bd80c51ae7
Benjamin Kober <lammermann@users.noreply.github.com>
2016-08-11 14:45:46 +0200

    Update init.lua

--- tools/cppHeaderParser/init.lua
+++ tools/cppHeaderParser/init.lua
@@ -1,1 +1,9 @@
+--
+-- Parse C++ header files and generate a data structure
+-- representing the class
+--
+-- This is heavily inspired by the similar python project.
+-- see: http://senexcanis.com/open-source/cppheaderparser/
+--
+
 local lpeg = require "lpeg"
