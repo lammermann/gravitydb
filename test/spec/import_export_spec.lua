fda093f0a1cb6703b268cfb7cbaf11496193c2c9
Benjamin Kober <lammermann@users.noreply.github.com>
2016-08-11 10:14:38 +0200

    added file does not exist test for better test coverage

--- test/spec/import_export_spec.lua
+++ test/spec/import_export_spec.lua
@@ -27,18 +27,39 @@ describe("gravity database", function()
     end)
     -- }}}
 
-    it("csv", function() -- {{{
-      local csv = require "gravity.import.csv"
-      csv("data/moses_nodes.csv", function(data)
-        g.createNode({name=data.name}, data.label)
+    context("csv", function() -- {{{
+
+      -- prepare environment {{{
+      before_each(function()
+        csv = require "gravity.import.csv"
       end)
-      assert.is.equal(3, g.V().count())
-      csv("data/moses_links.csv", function(data)
-        local n  = g.V().has("name", data.id1).n()
-        local n2 = g.V().has("name", data.id2).n()
-        n.addLink(n2, data.label, data.direction)
+
+      after_each(function()
+        csv = nil
       end)
-      assert.are.same(2,g.V().has("name", "aaron").outL("sibling_of").count())
+      -- }}}
+
+      it("import", function() -- {{{
+        csv("data/moses_nodes.csv", function(data)
+          g.createNode({name=data.name}, data.label)
+        end)
+        assert.is.equal(3, g.V().count())
+        csv("data/moses_links.csv", function(data)
+          local n  = g.V().has("name", data.id1).n()
+          local n2 = g.V().has("name", data.id2).n()
+          n.addLink(n2, data.label, data.direction)
+        end)
+        assert.are.same(2,g.V().has("name", "aaron").outL("sibling_of").count())
+      end) -- }}}
+
+    it("fire error on non existing file", function() -- {{{
+      assert.has.errors(function()
+        csv("does-not-exist.csv", function(data)
+          g.createNode({name=data.name}, data.label)
+        end)
+      end)
+    end) -- }}}
+
     end) -- }}}
 
   end) -- }}}
